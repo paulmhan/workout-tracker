3c4f4b0aaec55e757a3a9d4383fbb800ba751ef1
Paul Han <paul.myung.han@gmail.com>
2020-05-12 20:02:18 -0700

    server boilerplate

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const apiRoutes = require("./routes/apiRoutes.js");
+const htmlRoutes = require("./routes/htmlRoutes.js");
+
+// If we are in production (Heroku), process.env.PORT is true, 
+// If we are in development it is false, default to 3000
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+
+app.listen(PORT);
