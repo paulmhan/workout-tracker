671ec260ccd7e7feb1259a94d40a796595438160
Paul Han <paul.myung.han@gmail.com>
2020-05-13 15:44:23 -0700

    finished workout model

--- models/Workout.js
+++ models/Workout.js
@@ -5,5 +5,36 @@ const WorkoutSchema = new Schema({
         type: Date,
         default: () => new Date()
     },
-    
-})
+    exercise: [
+        {
+            name: {
+                type: String,
+                trime: true,
+                required: [true, "You must enter the name of the exercise."]
+            },
+            type: {
+                type: String,
+                trim: true,
+                required: [true, "You must enter the exercise type."]
+            },
+            duration: {
+                type: Number,
+                required: [true, "You must enter the duration of the exercise, in minutes."]
+            },
+            weight: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+        }
+    ]
+});
+
+module.exports = model("Workout", WorkoutSchema);
